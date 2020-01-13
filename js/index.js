b2dc438de73257f8f2a9204ae764e4a851af7674
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-13 17:12:00 -0500

    added navItems in line 51

--- js/index.js
+++ js/index.js
@@ -44,3 +44,14 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let midImg = document.getElementById("middle-img");
 midImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+
+
+//Navigation
+let navItems = document.querySelectorAll("a")
+navItems[0].textContent = siteContent["nav"] ["nav-item-1"]
+navItems[1].textContent = siteContent["nav"] ["nav-item-2"]
+navItems[2].textContent = siteContent["nav"] ["nav-item-3"]
+navItems[3].textContent = siteContent["nav"] ["nav-item-4"]
+navItems[4].textContent = siteContent["nav"] ["nav-item-5"]
+navItems[5].textContent = siteContent["nav"] ["nav-item-6"]
